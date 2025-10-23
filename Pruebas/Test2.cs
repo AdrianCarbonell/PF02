12c31591437054a41436998a59a6ba3a6d03364e
RobertoCarlosC <RobertoCarlosC>
2025-10-23 19:11:35 +0200

    Tests RED para EsContrasenyaValida

---
+++ Pruebas/Test2.cs
@@ -0,0 +1,48 @@
+﻿using PF02;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+        public class Test2
+        {
+            [TestMethod]
+            public void Contrasenya_Null_DeberiaRetornarFalse()
+            {
+                bool resultado = Proyecto.EsContrasenyaValida(null);
+                Assert.IsFalse(resultado);
+            }
+
+            [TestMethod]
+            public void Contrasenya_Vacia_DeberiaRetornarFalse()
+            {
+                bool resultado = Proyecto.EsContrasenyaValida("");
+                Assert.IsFalse(resultado);
+            }
+
+            [TestMethod]
+            public void Contrasenya_MuyCorta_DeberiaRetornarFalse()
+            {
+                bool resultado = Proyecto.EsContrasenyaValida("abc#12");
+                Assert.IsFalse(resultado);
+            }
+
+            [TestMethod]
+            public void Contrasenya_SinAlmohadilla_DeberiaRetornarFalse()
+            {
+                bool resultado = Proyecto.EsContrasenyaValida("password123");
+                Assert.IsFalse(resultado);
+            }
+
+            [TestMethod]
+            public void Contrasenya_Valida_DeberiaRetornarTrue()
+            {
+                bool resultado = Proyecto.EsContrasenyaValida("abc#12345");
+                Assert.IsTrue(resultado);
+            }
+        }
+    
+}
