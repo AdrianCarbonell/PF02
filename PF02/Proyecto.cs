20a5122ff200bc4ae44230472f5ce92e47d7f9bb
RobertoCarlosC <RobertoCarlosC>
2025-10-23 18:55:16 +0200

    Añadido Proyecto.cs y ajustes de test para factorial

---
+++ PF02/Proyecto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF02
+{
+    public class Proyecto
+    {
+
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine(CalcularFactorial(5));
+            }
+
+            public static long CalcularFactorial(int n)
+            {
+            if (n < 0)
+            {
+                Console.WriteLine("Número negativo, devolviendo -1");
+                return -1;
+            }
+
+            if (n == 0)
+            {
+                Console.WriteLine("Factorial de 0 es 1");
+                return 1;
+            }
+
+            if (n == 1)
+            {
+                Console.WriteLine("Factorial de 1 es 1");
+                return 1;
+            }
+
+            long factorial = 1;
+            long temp = 1;
+            int contador = 1;
+
+            while (contador <= n)
+            {
+                if (contador % 2 == 0)
+                {
+                    temp = contador; 
+                }
+                else
+                {
+                    temp = contador;
+                }
+
+                factorial = factorial * temp;
+
+                Console.WriteLine($"Multiplicando {temp}, factorial ahora es {factorial}");
+
+                contador++;
+            }
+
+            if (factorial < 0)
+            {
+                factorial = 1; 
+            }
+
+            return factorial;
+        }
+        
+    }
+}
--- Pruebas/Test1.cs
+++ Pruebas/Test1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PF02;
 
 namespace Pruebas
 {
@@ -8,42 +9,42 @@ namespace Pruebas
         [TestMethod]
         public void CalcularFactorial_Negativo_DeberiaRetornar_MenosUno()
         {
-            long resultado = Program.CalcularFactorial(-5);
+            long resultado = Proyecto.CalcularFactorial(-5);
             Assert.AreEqual(-1, resultado);
         }
 
         [TestMethod]
         public void CalcularFactorial_Cero_DeberiaRetornar_Uno()
         {
-            long resultado = Program.CalcularFactorial(0);
+            long resultado = Proyecto.CalcularFactorial(0);
             Assert.AreEqual(1, resultado);
         }
 
         [TestMethod]
         public void CalcularFactorial_Uno_DeberiaRetornar_Uno()
         {
-            long resultado = Program.CalcularFactorial(1);
+            long resultado = Proyecto.CalcularFactorial(1);
             Assert.AreEqual(1, resultado);
         }
 
         [TestMethod]
         public void CalcularFactorial_Tres_DeberiaRetornar_Seis()
         {
-            long resultado = Program.CalcularFactorial(3);
+            long resultado = Proyecto.CalcularFactorial(3);
             Assert.AreEqual(6, resultado);
         }
 
         [TestMethod]
         public void CalcularFactorial_Cinco_DeberiaRetornar_CientoVeinte()
         {
-            long resultado = Program.CalcularFactorial(5);
+            long resultado = Proyecto.CalcularFactorial(5);
             Assert.AreEqual(120, resultado);
         }
 
         [TestMethod]
         public void CalcularFactorial_Diez_DeberiaRetornar_3628800()
         {
-            long resultado = Program.CalcularFactorial(10);
+            long resultado = Proyecto.CalcularFactorial(10);
             Assert.AreEqual(3628800, resultado);
         }
     }
